5254b9af420b7fc9acf87d67b74ba19c9fa9181e
Volodya22211 <Volodymyr.Baran.RI.2023@lpnu.ua>
2023-10-31 21:36:35 +0200

    asasdawdwa

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 5.3/Lab 5.3.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = s(2);
+			Assert::AreEqual(t, -18.0221, 0.0001);
+		}
+	};
+}
